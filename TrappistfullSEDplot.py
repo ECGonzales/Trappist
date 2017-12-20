1bf97f1fcce22c96eaf6053fe5f6c80c2be333f4
eileengonzales <gonza338@gmail.com>
2017-12-20 15:14:46 -0500

    SED with SXD spectra

--- TrappistfullSEDplot.py
+++ TrappistfullSEDplot.py
@@ -19,9 +19,9 @@ df2 = pd.read_csv('Data/2306-0502 (M7.5) phot.txt', sep=" ", comment='#', names=
 
 # -------- Generate spectral regime SED plot ---------------------------
 # Make subarrays for color coding
-opt = df[(df['w'] <= 0.94105)]
-overlap = df[(df['w'] >= 0.941040) & (df['w'] <= 1.037)]
-nir = df[(df['w'] >= 1.03)]
+opt = df[(df['w'] <= 0.8)]
+overlap = df[(df['w'] >= 0.733791) & (df['w'] <= 0.8)]
+nir = df[(df['w'] >= .733791)]
 
 # ----- Setup plot layout ---------
 fig = plt.figure()
@@ -34,19 +34,19 @@ for axis in ['top', 'bottom', 'left', 'right']:  # Thicken the frame
 # ----- Plot Spectra -----------
 ax1.loglog(opt['w'], opt['f'], c='#0179FF')
 ax1.loglog(overlap['w'], overlap['f'], c='#009B45', lw=5, alpha=0.5)
-ax1.loglog(overlap['w'], overlap['f'], c='#0179FF', alpha=0.8)  # alpha=0.3 for transparency
+ax1.loglog(overlap['w'], overlap['f'], c='#0179FF', alpha=0.85)  # alpha=0.3 for transparency
 ax1.loglog(nir['w'], nir['f'], c='#009B45')
 
 # ----- Plot Photometric points -----
-ax1.scatter(df2['w'][0], df2['f'][0],  c='#ae017e', s=150)  # 2MASS_H
-ax1.scatter(df2['w'][1], df2['f'][1],  c='#ae017e', s=150)  # 2MASS_J
-ax1.scatter(df2['w'][2], df2['f'][2],  c='#ae017e', s=150)  # 2MASS_K
-ax1.scatter(df2['w'][3], df2['f'][3],  c='#dd3497', s=150)  # Cousins_I
-ax1.scatter(df2['w'][4], df2['f'][4],  c='#dd3497', s=150)  # Cousins_R
-ax1.scatter(df2['w'][5], df2['f'][5],  c='#f768a1', s=150)  # Johnson_V
-ax1.scatter(df2['w'][6], df2['f'][6],  c='#7a0177', s=150)  # WISE_W1
-ax1.scatter(df2['w'][7], df2['f'][7],  c='#7a0177', s=150)  # WISE_W2
-ax1.scatter(df2['w'][8], df2['f'][8],  c='#7a0177', s=150)  # WISE_W3
+ax1.scatter(df2['w'][0], df2['f'][0],  c='#ae017e', s=150, zorder=6)  # 2MASS_H
+ax1.scatter(df2['w'][1], df2['f'][1],  c='#ae017e', s=150, zorder=6)  # 2MASS_J
+ax1.scatter(df2['w'][2], df2['f'][2],  c='#ae017e', s=150, zorder=6)  # 2MASS_K
+ax1.scatter(df2['w'][3], df2['f'][3],  c='#dd3497', s=150, zorder=6)  # Cousins_I
+ax1.scatter(df2['w'][4], df2['f'][4],  c='#dd3497', s=150, zorder=6)  # Cousins_R
+ax1.scatter(df2['w'][5], df2['f'][5],  c='#f768a1', s=150, zorder=6)  # Johnson_V
+ax1.scatter(df2['w'][6], df2['f'][6],  c='#7a0177', s=150, zorder=6)  # WISE_W1
+ax1.scatter(df2['w'][7], df2['f'][7],  c='#7a0177', s=150, zorder=6)  # WISE_W2
+ax1.scatter(df2['w'][8], df2['f'][8],  c='#7a0177', s=150, zorder=6)  # WISE_W3
 
 # ----- Set axes limits, reformat ticks -----------
 plt.xlim([0.5, 12.5])
@@ -62,7 +62,7 @@ plt.xlabel('Wavelength ($\mu m$)', fontsize=25)
 plt.ylabel('Flux  ($erg\ s^{-1} cm^{-2} A^{-1}$)', fontsize=25)
 
 # ------ Labeling Spectra and Photometric points --------
-ax1.annotate('R-C Spec', xy=(0.75, 0.2*10**(-15)), color='#0179FF', fontsize=15)
+ax1.annotate('R-C Spec', xy=(0.78, 0.2*10**(-15)), color='#0179FF', fontsize=15)
 ax1.annotate('SpeX', xy=(1.7, 0.2*10**(-14)), color='#009B45', fontsize=15)
 ax1.annotate('V', xy=(0.53, 0.2*10**(-15)), color='#f768a1', fontsize=15)
 ax1.annotate('R', xy=(0.62, 0.97*10**(-15)), color='#dd3497', fontsize=15)

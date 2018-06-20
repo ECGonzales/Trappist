68e00083c0c39077dbd234997b462febfd4c6b6b
eileengonzales <gonza338@gmail.com>
2018-06-20 17:20:24 -0400

    Beta teff sample added

--- Beta_teff.py
+++ Beta_teff.py
@@ -12,13 +12,13 @@ df_trap = pd.read_csv('Data/Gaia2306-0502 (M7.5) SED.txt', sep=" ", comment='#',
 df_trap_phot = pd.read_csv('Data/Gaia2306-0502 (M7.5) phot.txt', sep=" ", comment='#', header=None,
                            names=["w", "f", "err"])
 # -------------- Comparison objects of the same Teff (betas) ----------------------------------
-df_0253 = pd.read_csv('Data/young_comp/Gaia0253+3206 (M7beta) SED.txt', sep=" ", comment='#', header=None,
+df_0253 = pd.read_csv('Data/beta_comp/Gaia0253+3206 (M7beta) SED.txt', sep=" ", comment='#', header=None,
                       names=["w", "f", "err"])
-df_0253_phot = pd.read_csv('Data/young_comp/Gaia0253+3206 (M7beta) phot.txt', sep=" ", comment='#', header=None,
+df_0253_phot = pd.read_csv('Data/beta_comp/Gaia0253+3206 (M7beta) phot.txt', sep=" ", comment='#', header=None,
                            names=["w", "f", "err"])
-df_0436 = pd.read_csv('Data/young_comp/Gaia0436-4114 (M8betagamma) SED.txt', sep=" ", comment='#', header=None,
+df_0436 = pd.read_csv('Data/beta_comp/Gaia0436-4114 (M8betagamma) SED.txt', sep=" ", comment='#', header=None,
                       names=["w", "f", "err"])
-df_0436_phot = pd.read_csv('Data/young_comp/Gaia0436-4114 (M8betagamma) phot.txt', sep=" ", comment='#', header=None,
+df_0436_phot = pd.read_csv('Data/beta_comp/Gaia0436-4114 (M8betagamma) phot.txt', sep=" ", comment='#', header=None,
                            names=["w", "f", "err"])
 
 # -------------------------------------------------------------------------------------
@@ -37,20 +37,20 @@ ax1.loglog(df_trap['w'], df_trap['f'], c='k', zorder=22)
 trap = ax1.scatter(df_trap_phot['w'], df_trap_phot['f'], c='k', s=70, zorder=23)
 
 # Comparisons (Hot-->cool)
-ax1.loglog(df_0253['w'], df_0253['f'], c='#E80901')
+ax1.loglog(df_0253['w'], df_0253['f'], c='#D01810')
 ax1.scatter(df_0253_phot['w'], df_0253_phot['f'], c='k', s=70)
-y0253 = ax1.scatter(df_0253_phot['w'], df_0253_phot['f'], c='#E80901', s=50)
+y0253 = ax1.scatter(df_0253_phot['w'], df_0253_phot['f'], c='#D01810', s=50)
 
-ax1.loglog(df_0436['w'], df_0436['f'], c='#FF2E06')
+ax1.loglog(df_0436['w'], df_0436['f'], c='#F58404')
 ax1.scatter(df_0436_phot['w'], df_0436_phot['f'], c='k', s=70)
-y0436 = ax1.scatter(df_0436_phot['w'], df_0436_phot['f'], c='#FF2E06', s=50)
+y0436 = ax1.scatter(df_0436_phot['w'], df_0436_phot['f'], c='#F58404', s=50)
 
 # ----- Set axes limits, reformat ticks -----------
-plt.xlim([0.3, 23])
-plt.ylim([10**(-18), 5*10**(-14)])
+plt.xlim([0.3, 14])
+plt.ylim([10**(-17), 5*10**(-14)])
 ax1.xaxis.set_major_formatter(ScalarFormatter())
 ax1.xaxis.set_minor_formatter(ScalarFormatter())
-ax1.xaxis.set_minor_locator(plt.FixedLocator([0.3, 0.7, 2, 3, 7.5, 23]))
+ax1.xaxis.set_minor_locator(plt.FixedLocator([0.3, 0.7, 2, 3, 7.5, 14]))
 ax1.tick_params(axis='both', which='major', labelsize=20, length=8, width=1.1)
 ax1.tick_params(axis='both', which='minor', labelsize=20, length=4, width=1.1)
 plt.yticks(fontsize=20)
@@ -61,8 +61,8 @@ plt.ylabel('Flux ($erg\ s^{-1} cm^{-2} A^{-1}$)', fontsize=25)
 plt.tight_layout()
 
 # ------ Labeling Objects --------
-ax1.annotate('Trappist-1 (M7.5), $T_\mathrm{eff}: 2582 \pm 34$ K', xy=(2.9, 8*10**(-18)), color='k', fontsize=15)
-ax1.annotate('J0253+3206 (M7$\\beta$), $T_\mathrm{eff}: 2581 \pm 265$ K', xy=(2.58, 3*10**(-14)), color='#E80901', fontsize=15)
-ax1.annotate('J0436-4114 (M8 $\\beta$), $T_\mathrm{eff}: 2557 \pm 257$ K', xy=(2.64,  7*10**(-15)), color='#FF2E06', fontsize=15)
+ax1.annotate('Trappist-1 (M7.5), $T_\mathrm{eff}: 2582 \pm 34$ K', xy=(2, 3*10**(-14)), color='k', fontsize=15)
+ax1.annotate('J0253+3206 (M7$\\beta$), $T_\mathrm{eff}: 2581 \pm 265$ K', xy=(2, 2*10**(-14)), color='#D01810', fontsize=15)
+ax1.annotate('J0436-4114 (M8 $\\beta$), $T_\mathrm{eff}: 2557 \pm 257$ K', xy=(2, 1.3*10**(-14)), color='#F58404', fontsize=15)
 
 plt.savefig('Figures/beta_teff.png', dpi=150)

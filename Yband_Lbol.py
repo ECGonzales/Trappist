dcc9770490a6eb78507b974c3a341644a5757a19
eileengonzales <gonza338@gmail.com>
2018-06-22 13:43:48 -0400

    Finsed all Y band figures. Moved stacks into Old Figures folder

--- Yband_Lbol.py
+++ Yband_Lbol.py
@@ -7,17 +7,17 @@ import numpy as np
 # ------------------------------------------------------------------------------------
 # Read  all in as pandas dataframes
 
-df_trap = pd.read_csv('Data/FIRE2306-0502 (M7.5) SED.txt', sep=" ", comment='#', header=None,
+df_trap = pd.read_csv('Data/Gaia2306-0502 (M7.5) SED.txt', sep=" ", comment='#', header=None,
                       names=["w", "f", "err"])
-df_trap_phot = pd.read_csv('Data/FIRE2306-0502 (M7.5) phot.txt', sep=" ",comment='#', header=None, names=["w", "f", "err"])
+df_trap_phot = pd.read_csv('Data/Gaia2306-0502 (M7.5) phot.txt', sep=" ",comment='#', header=None, names=["w", "f", "err"])
 
 # ---- Field ----
-df_vb8 = pd.read_csv('Data/field_comp/1655-0823 (M7) SED.txt', sep=" ", comment='#', header=None,
+df_vb8 = pd.read_csv('Data/field_comp/Gaia1655-0823 (M7) SED.txt', sep=" ", comment='#', header=None,  # vb8
                        names=["w", "f", "err"])
-df_vb10 = pd.read_csv('Data/field_comp/1916+0508 (M8) SED.txt', sep=" ", comment='#', header=None,
-                       names=["w", "f", "err"])
-df_0320 = pd.read_csv('Data/field_comp/0320+1854 (M8) SED.txt', sep=" ", comment='#', header=None,
-                       names=["w", "f", "err"])
+df_vb10 = pd.read_csv('Data/field_comp/Gaia1916+0508 (M8) SED.txt', sep=" ", comment='#', header=None,
+                      names=["w", "f", "err"])
+df_0320 = pd.read_csv('Data/field_comp/Gaia0320+1854 (M8) SED.txt', sep=" ", comment='#', header=None,
+                      names=["w", "f", "err"])
 # --- Young ------
 df_1207 = pd.read_csv('Data/young_comp/1207-3900 (L0gamma) SED.txt', sep=" ", comment='#', header=None,
                       names=["w", "f", "err"])
@@ -64,10 +64,10 @@ norm_df_0518 = df_0518['f']/(np.average(norm_region7['f']))
 # ------ Set up figure layout --------
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
-fig.set_size_inches(10, 8)
+fig.set_size_inches(8.5, 11)
 plt.gcf().subplots_adjust(bottom=0.15, left=0.15)
 plt.xlim([0.95, 1.10])
-plt.ylim([0.5, 3.7])
+plt.ylim([0.5, 9.3])
 for axis in ['top', 'bottom', 'left', 'right']:  # Thicken the frame
     ax1.spines[axis].set_linewidth(1.1)
 
@@ -77,69 +77,68 @@ plt.xlabel('Wavelength ($\mu$m)', fontsize=25)
 plt.ylabel('Normalized Flux ($F_\lambda$)', fontsize=25)
 
 # -------- Add data -----------
-ax1.plot(df_0320['w'], norm_df_0320 + 3.5, c='#6A777F')
-ax1.annotate('J0320+1854 (M8) $L_\mathrm{bol}: -3.337 \pm 0.053$', xy=(0.951, 4.65), color='#6A777F', fontsize=13)
-
-ax1.plot(df_vb8['w'], norm_df_vb8 + 4.3, c='#7C7D70')
-ax1.annotate('vb8 (M7) $L_\mathrm{bol}: -3.337 \pm 0.053$', xy=(0.951, 5.4), color='#7C7D70', fontsize=13)
-
-
-ax1.plot(df_0320['w'], norm_df_0320 + 3.5, c='#6A777F')
-ax1.annotate('J0320+1854 (M8) $L_\mathrm{bol}: -3.337 \pm 0.053$', xy=(0.951, 4.65), color='#6A777F', fontsize=13)
-
-
-ax1.plot(df_trap['w'], norm_df_trap, c='k')
-ax1.annotate('Trappist-1 (M7.5) $L_\mathrm{bol}: -3.337 \pm 0.053$', xy=(0.951, 3.7), color='k', fontsize=13)
-
-ax1.plot(df_vb10['w'], norm_df_vb10 + 1.8, c='#A0B2BF')
-ax1.annotate('vb10 (M8) $L_\mathrm{bol}: -3.301 \pm 0.002$', xy=(0.951, 2.9), color='#A0B2BF', fontsize=13)
-
-ax1.plot(df_0518['w'], norm_df_0518 + 4.3, c='#7C7D70')
-ax1.annotate('J0518-2756 (L1 $\gamma$) $L_\mathrm{bol}: -3.328 \pm 0.041$', xy=(0.951, 5.4), color='#7C7D70', fontsize=13)
-
-ax1.plot(df_1207['w'], norm_df_1207 + 1.8, c='#A0B2BF')
-ax1.annotate('J1207-3900 (L0 $\gamma$) $L_\mathrm{bol}: -3.337 \pm 0.053$', xy=(0.951, 2.9), color='#A0B2BF', fontsize=13)
+# 1207
+ax1.plot(df_1207['w'], norm_df_1207, c='#0E0084')
+ax1.annotate('J1207-3900 (L0 $\gamma$) $L_\mathrm{bol}: -3.337 \pm 0.053$', xy=(0.951, 1.5), color='#0E0084', fontsize=13)
+# 0518
+ax1.plot(df_0518['w'], norm_df_0518 + 1.2, c='#E806B7')
+ax1.annotate('J0518-2756 (L1 $\gamma$) $L_\mathrm{bol}: -3.328 \pm 0.041$', xy=(0.951, 2.9), color='#E806B7', fontsize=13)
+# vb10
+ax1.plot(df_vb10['w'], norm_df_vb10 + 2.6, c='#A0B2BF')
+ax1.annotate('vb10 (M8) $L_\mathrm{bol}: -3.301 \pm 0.002$', xy=(0.951, 3.8), color='#A0B2BF', fontsize=13)
+# Trappist
+ax1.plot(df_trap['w'], norm_df_trap + 3.5, c='k')
+ax1.annotate('Trappist-1 (M7.5) $L_\mathrm{bol}: -3.254 \pm 0.002$', xy=(0.951, 4.8), color='k', fontsize=13)
+# 320
+ax1.plot(df_0320['w'], norm_df_0320 + 4.5, c='#6A777F')
+ax1.annotate('J0320+1854 (M8) $L_\mathrm{bol}: -3.225 \pm 0.009$', xy=(0.951, 5.7), color='#6A777F', fontsize=13)
+# 0443
+ax1.plot(df_0443['w'], norm_df_0443 + 5.5, c='#8E01E8')
+ax1.annotate('J0443+0002 (M9 $\gamma$) $L_\mathrm{bol}: -3.207 \pm 0.019$', xy=(0.951, 6.9), color='#8E01E8', fontsize=13)
+# vb8
+ax1.plot(df_vb8['w'], norm_df_vb8 + 6.5, c='#7C7D70')
+ax1.annotate('vb8 (M7) $L_\mathrm{bol}: -3.2 \pm 0.007$', xy=(0.951, 7.7), color='#7C7D70', fontsize=13)
 
 # --- To make lines for features ---------
 FeH1 = pd.DataFrame()
 FeH1['x'] = [0.9896, 1.0]
-FeH1['y'] = [2.8, 2.8]
+FeH1['y'] = [8.2, 8.2]
 plt.plot(FeH1['x'], FeH1['y'], color='k')
-ax1.annotate('FeH', xy=(0.9896, 2.81), color='k', fontsize=15)
+ax1.annotate('FeH', xy=(0.9896, 8.21), color='k', fontsize=15)
 # -- To make a vertical line
 FeH1d = pd.DataFrame()
 FeH1d['x'] = [0.9896, 0.9896]
-FeH1d['y'] = [2.7, 2.8]
+FeH1d['y'] = [8, 8.2]
 plt.plot(FeH1d['x'], FeH1d['y'], color='k')
 
 FeH2 = pd.DataFrame()
 FeH2['x'] = [0.998, 1.085]
-FeH2['y'] = [3.15, 3.15]
+FeH2['y'] = [8.7, 8.7]
 plt.plot(FeH2['x'], FeH2['y'], color='k')
-ax1.annotate('FeH', xy=(1.03, 3.17), color='k', fontsize=15)
+ax1.annotate('FeH', xy=(1.03, 8.71), color='k', fontsize=15)
 FeH2d = pd.DataFrame()
 FeH2d['x'] = [0.998, 0.998]
-FeH2d['y'] = [3, 3.15]
+FeH2d['y'] = [8.5, 8.7]
 plt.plot(FeH2d['x'], FeH2d['y'], color='k')
 
 VO = pd.DataFrame()
 VO['x'] = [1.0456, 1.08]
-VO['y'] = [3.3, 3.3]
+VO['y'] = [8.2, 8.2]
 plt.plot(VO['x'], VO['y'], color='k')
-ax1.annotate('VO', xy=(1.06, 3.31), color='k', fontsize=15)
+ax1.annotate('VO', xy=(1.06, 8.21), color='k', fontsize=15)
 VOd = pd.DataFrame()
 VOd['x'] = [1.0456, 1.0456]
-VOd['y'] = [3.3, 3.2]
+VOd['y'] = [8, 8.2]
 plt.plot(VOd['x'], VOd['y'], color='k')
 
 H2O = pd.DataFrame()
 H2O['x'] = [1.08, 1.099]
-H2O['y'] = [3.5, 3.5]
+H2O['y'] = [9, 9]
 plt.plot(H2O['x'], H2O['y'], color='k')
-ax1.annotate('H$_\mathrm{2} $O', xy=(1.085, 3.55), color='k', fontsize=15)
+ax1.annotate('H$_\mathrm{2} $O', xy=(1.085, 9.05), color='k', fontsize=15)
 H2Od = pd.DataFrame()
 H2Od['x'] = [1.08, 1.08]
-H2Od['y'] = [3.35, 3.5]
+H2Od['y'] = [8.8, 9]
 plt.plot(H2Od['x'], H2Od['y'], color='k')
 
 plt.tight_layout()

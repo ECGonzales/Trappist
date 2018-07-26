2a8f3d97e9b9b83bb4584e115539760d4eab94c2
eileengonzales <gonza338@gmail.com>
2018-07-26 16:47:20 -0400

    Added TRAPPIST to legend instead

--- SptvsLbol.py
+++ SptvsLbol.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 # -------- Read in the data --------
 df_trappist = pd.read_csv('Data/trappist_vsSptparams.txt', sep="\s+", comment='#', header=None,
-                     names=["name", "SpT", "Teff", 'Teff_err', 'lbol', 'lbol_err', 'mass', 'mass_unc', 'MJ', 'MJ_unc',
-                            'MH', 'MH_unc', 'MK', 'MK_unc', 'MW1', 'MW1_unc', 'MW2', 'MW2_unc'])
+                          names=["name", "SpT", "Teff", 'Teff_err', 'lbol', 'lbol_err', 'mass', 'mass_unc', 'MJ',
+                                 'MJ_unc', 'MH', 'MH_unc', 'MK', 'MK_unc', 'MW1', 'MW1_unc', 'MW2', 'MW2_unc'])
 df_sub = pd.read_csv('../Atmospheres_paper/Data/Subdwarf_Spt_vs_Teff_new.txt', sep="\s+", comment='#', header=None,
                      names=["name", "SpT", "Teff", 'Teff_err', 'lbol', 'lbol_err', 'mass', 'mass_unc', 'MJ', 'MJ_unc',
                             'MH', 'MH_unc', 'MK', 'MK_unc', 'MW1', 'MW1_unc', 'MW2', 'MW2_unc'])
-df_comb = pd.read_csv('../Atmospheres_paper/Data/Lbol+Teff-February2017_updated.txt', sep="\s+", comment='#', header=None,
-                      names=["name", "Lbol", "Lbol_err", 'Teff', 'Teff_err', 'spt', 'spt_unc', 'group', 'grav'])
+df_comb = pd.read_csv('../Atmospheres_paper/Data/Lbol+Teff-February2017_updated.txt', sep="\s+", comment='#',
+                      header=None, names=["name", "Lbol", "Lbol_err", 'Teff', 'Teff_err', 'spt', 'spt_unc', 'group',
+                                          'grav'])
 
 # ----- Remove the -100 -----------------
 # df_comb.loc[df_comb['grav'] == -100]
@@ -36,7 +36,7 @@ plt.ylim([-5, -2.3])
 
 # ------ Axes Labels --------
 ax1.tick_params(axis='both', labelsize=20, length=8, width=1.1)
-plt.xticks([6, 8, 10, 12, 14, 16, 18], ['M6','M8', 'L0', 'L2', 'L4', 'L6', 'L8'], fontsize=20)
+plt.xticks([6, 8, 10, 12, 14, 16, 18], ['M6', 'M8', 'L0', 'L2', 'L4', 'L6', 'L8'], fontsize=20)
 plt.xlabel('Spectral Type', fontsize=25)
 plt.ylabel('log($L_\mathrm{bol}$/$L_\odot$)', fontsize=25)
 
@@ -51,11 +51,17 @@ sub = plt.scatter(df_sub['SpT'], df_sub['lbol'], color='blue', zorder=7, s=70)
 ax1.errorbar(df_sub['SpT'], df_sub['lbol'], yerr=df_sub['lbol_err'], c='blue', fmt='o', zorder=8)
 
 # --- Designate Trappist-1 -----
-plt.scatter(df_trappist['SpT'], df_trappist['lbol'], color='k', s=700, zorder=9, marker="*")
-ax1.annotate('Trappist-1', xy=(7, -3.8), color='k', fontsize=12)
+trap = plt.scatter(df_trappist['SpT'], df_trappist['lbol'], color='k', s=700, zorder=9, marker="*")
+# -- pointer---
+# pointer = pd.DataFrame()
+# pointer['x'] = [6, 7.2]
+# pointer['y'] = [-3.68, -3.35]
+# ax1.plot(pointer['x'], pointer['y'], color='k')
+#
+# ax1.annotate('TRAPPIST-1', xy=(5.6, -3.8), color='k', fontsize=12)
 
 # ---- Add Legend ----
-plt.legend([fld, gamma, beta, sub], ["Field", "$\gamma$", '$\\beta$', 'Subdwarfs'], frameon=False, fontsize=12)
+plt.legend([fld, gamma, beta, sub, trap], ["Field", "$\gamma$", '$\\beta$', 'Subdwarfs', 'TRAPPIST-1'], frameon=False, fontsize=12)
 plt.tight_layout()
 
-plt.savefig('Figures/Spt_vs_Lbol.pdf',dpi=150)
+plt.savefig('Figures/Spt_vs_Lbol.pdf', dpi=150)

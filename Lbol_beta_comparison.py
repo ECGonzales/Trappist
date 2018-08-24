b48fadece2b382d8c3dab16c5f9cf4c1e3b31c14
eileengonzales <gonza338@gmail.com>
2018-08-24 16:52:21 -0400

    NIR SPTs

--- Lbol_beta_comparison.py
+++ Lbol_beta_comparison.py
@@ -16,10 +16,10 @@ df_2235 = pd.read_csv('Data/paper1_trappist/Lbol_comp/Gaia2235-5906 (M8.5beta) S
                       header=None, names=["w", "f", "err"])
 df_2235_phot = pd.read_csv('Data/beta_comp/Gaia2235-5906 (M8.5beta) phot.txt', sep=" ", comment='#', header=None,
                             names=["w", "f", "err"])
-df_2154 = pd.read_csv('Data/paper1_trappist/Lbol_comp/Gaia2154-7459 (M9.5beta) SED_spexified.txt', sep=" ", comment='#',
-                      header=None, names=["w", "f", "err"])
-df_2154_phot = pd.read_csv('Data/beta_comp/Gaia2154-7459 (M9.5beta) phot.txt', sep=" ", comment='#', header=None,
-                            names=["w", "f", "err"])
+# df_2154 = pd.read_csv('Data/paper1_trappist/Lbol_comp/Gaia2154-7459 (M9.5beta) SED_spexified.txt', sep=" ", comment='#',
+#                       header=None, names=["w", "f", "err"])
+# df_2154_phot = pd.read_csv('Data/beta_comp/Gaia2154-7459 (M9.5beta) phot.txt', sep=" ", comment='#', header=None,
+#                             names=["w", "f", "err"])
 df_0714 = pd.read_csv('Data/paper1_trappist/Lbol_comp/Gaia0714+3702 (M8) SED_spexified.txt', sep=" ", comment='#',
                       header=None, names=["w", "f", "err"])
 df_0714_phot = pd.read_csv('Data/beta_comp/Gaia0714+3702 (M8) phot.txt', sep=" ", comment='#', header=None,
@@ -43,9 +43,9 @@ ax1.scatter(df_trap_phot['w'], df_trap_phot['f'], c='k', s=70, zorder=6)
 ax1.loglog(df_2235['w'], df_2235['f'], c='#8E01E8', zorder=1)
 ax1.scatter(df_2235_phot['w'], df_2235_phot['f'], c='k', s=70, zorder=4)
 ax1.scatter(df_2235_phot['w'], df_2235_phot['f'], c='#8E01E8', s=50, zorder=5)
-ax1.loglog(df_2154['w'], df_2154['f'], c='#E806B7', zorder=2)
-ax1.scatter(df_2154_phot['w'], df_2154_phot['f'], c='k', s=70, zorder=4)
-ax1.scatter(df_2154_phot['w'], df_2154_phot['f'], c='#E806B7', s=50,zorder=5)
+# ax1.loglog(df_2154['w'], df_2154['f'], c='#E806B7', zorder=2)
+# ax1.scatter(df_2154_phot['w'], df_2154_phot['f'], c='k', s=70, zorder=4)
+# ax1.scatter(df_2154_phot['w'], df_2154_phot['f'], c='#E806B7', s=50,zorder=5)
 ax1.loglog(df_0714['w'], df_0714['f'], c='#0E0084', zorder=7)
 ax1.scatter(df_0714_phot['w'], df_0714_phot['f'], c='k', s=70, zorder=4)
 ax1.scatter(df_0714_phot['w'], df_0714_phot['f'], c='#0E0084', s=50,zorder=5)
@@ -74,9 +74,9 @@ ax1.annotate('J2235-5906 (M8.5 $\\beta$)', xy=(1.3, 8*10**(-16)), color='#8E01E8
 ax1.annotate('Age: 41-49 Myr (Tuc-Hor)', xy=(1.3, 5*10**(-16)), color='#8E01E8', fontsize=15)
 ax1.annotate('$L_\mathrm{bol}: -3.214 \pm 0.014$', xy=(1.3, 3.4*10**(-16)), color='#8E01E8', fontsize=15)
 # J2154
-ax1.annotate('J2154-7459 (M9.5 $\\beta$)', xy=(4, 2.3*10**(-15)), color='#E806B7', fontsize=15)
-ax1.annotate('Age: 41-49 Myr (Tuc-Hor)', xy=(4, 1.5*10**(-15)), color='#E806B7', fontsize=15)
-ax1.annotate('$L_\mathrm{bol}: -3.196 \pm 0.012$', xy=(4, 10**(-15)), color='#E806B7', fontsize=15)
+# ax1.annotate('J2154-7459 (M9.5 $\\beta$)', xy=(4, 2.3*10**(-15)), color='#E806B7', fontsize=15)
+# ax1.annotate('Age: 41-49 Myr (Tuc-Hor)', xy=(4, 1.5*10**(-15)), color='#E806B7', fontsize=15)
+# ax1.annotate('$L_\mathrm{bol}: -3.196 \pm 0.012$', xy=(4, 10**(-15)), color='#E806B7', fontsize=15)
 #0714
 ax1.annotate('J0714+3702 (M8)', xy=(0.8, 4*10**(-17)), color='#0E0084', fontsize=15)
 ax1.annotate('Age: <500 Myr', xy=(0.8, 2.5*10**(-17)), color='#0E0084', fontsize=15)

04a488adf65a4cbff8d123222715181818ab489f
eileengonzales <gonza338@gmail.com>
2018-09-04 19:32:55 -0400

    Added eagan good box

--- UVW_figures.py
+++ UVW_figures.py
@@ -72,6 +72,12 @@ ax1.errorbar(df_beta_nogroup['U'], df_beta_nogroup['V'], xerr=df_beta_nogroup['d
 trappist = plt.scatter(trap_U, trap_V, color='k', s=700, marker='*')
 ax1.errorbar(trap_U, trap_V, xerr=trap_dU,yerr=trap_dV, c='k', fmt='o')
 
+# Add eagan good box line
+plt.hlines(-50, -30, 0)
+plt.hlines(20, -30, 0)
+plt.vlines(-30, -50, 20)
+plt.vlines(0, -50, 20)
+
 plt.legend([gamma_in, gamma_out, beta_out, trappist], ['$\gamma$ in group', '$\gamma$ not in group',
                                                        '$\\beta$ not in group', 'TRAPPIST-1'], frameon=False, fontsize=12)
 plt.tight_layout()

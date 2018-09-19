d3f69c0fa724e478e2710d4dd30f27e30b297a89
eileengonzales <gonza338@gmail.com>
2018-09-19 14:20:20 +0100

    pep-8 edits

--- Lbol_beta_comparison.py
+++ Lbol_beta_comparison.py
@@ -9,21 +9,22 @@ from matplotlib.ticker import ScalarFormatter
 
 df_trap = pd.read_csv('Data/paper1_trappist/Lbol_comp/Gaia2306-0502 (M7.5) SED_spexified.txt', sep=" ", comment='#',
                       header=None, names=["w", "f", "err"])
-df_trap_phot = pd.read_csv('Data/Gaia2306-0502 (M7.5) phot.txt', sep=" ",comment='#', header=None, names=["w", "f", "err"])
+df_trap_phot = pd.read_csv('Data/Gaia2306-0502 (M7.5) phot.txt', sep=" ", comment='#', header=None,
+                           names=["w", "f", "err"])
 
 # -------------- Comparison objects of the same Teff ----------------------------------
 df_2235 = pd.read_csv('Data/paper1_trappist/Lbol_comp/Gaia2235-5906 (M8.5beta) SED_spexified.txt', sep=" ", comment='#',
                       header=None, names=["w", "f", "err"])
 df_2235_phot = pd.read_csv('Data/beta_comp/Gaia2235-5906 (M8.5beta) phot.txt', sep=" ", comment='#', header=None,
-                            names=["w", "f", "err"])
-# df_2154 = pd.read_csv('Data/paper1_trappist/Lbol_comp/Gaia2154-7459 (M9.5beta) SED_spexified.txt', sep=" ", comment='#',
-#                       header=None, names=["w", "f", "err"])
+                           names=["w", "f", "err"])
+# df_2154 = pd.read_csv('Data/paper1_trappist/Lbol_comp/Gaia2154-7459 (M9.5beta) SED_spexified.txt', sep=" ",
+#                        comment='#', header=None, names=["w", "f", "err"])
 # df_2154_phot = pd.read_csv('Data/beta_comp/Gaia2154-7459 (M9.5beta) phot.txt', sep=" ", comment='#', header=None,
 #                             names=["w", "f", "err"])
 df_0714 = pd.read_csv('Data/paper1_trappist/Lbol_comp/Gaia0714+3702 (M8) SED_spexified.txt', sep=" ", comment='#',
                       header=None, names=["w", "f", "err"])
 df_0714_phot = pd.read_csv('Data/beta_comp/Gaia0714+3702 (M8) phot.txt', sep=" ", comment='#', header=None,
-                            names=["w", "f", "err"])
+                           names=["w", "f", "err"])
 
 
 # -------------------------------------------------------------------------------------
@@ -48,7 +49,7 @@ ax1.scatter(df_2235_phot['w'], df_2235_phot['f'], c='#8E01E8', s=50, zorder=5)
 # ax1.scatter(df_2154_phot['w'], df_2154_phot['f'], c='#E806B7', s=50,zorder=5)
 ax1.loglog(df_0714['w'], df_0714['f'], c='#0E0084', zorder=7)
 ax1.scatter(df_0714_phot['w'], df_0714_phot['f'], c='k', s=70, zorder=4)
-ax1.scatter(df_0714_phot['w'], df_0714_phot['f'], c='#0E0084', s=50,zorder=5)
+ax1.scatter(df_0714_phot['w'], df_0714_phot['f'], c='#0E0084', s=50, zorder=5)
 
 # ----- Set axes limits, reformat ticks -----------
 plt.xlim([0.5, 12])
@@ -77,7 +78,7 @@ ax1.annotate('$L_\mathrm{bol}: -3.214 \pm 0.014$', xy=(1.3, 3.4*10**(-16)), colo
 # ax1.annotate('J2154-7459 (M9.5 $\\beta$)', xy=(4, 2.3*10**(-15)), color='#E806B7', fontsize=15)
 # ax1.annotate('Age: 41-49 Myr (Tuc-Hor)', xy=(4, 1.5*10**(-15)), color='#E806B7', fontsize=15)
 # ax1.annotate('$L_\mathrm{bol}: -3.196 \pm 0.012$', xy=(4, 10**(-15)), color='#E806B7', fontsize=15)
-#0714
+# 0714
 ax1.annotate('J0714+3702 (M8)', xy=(0.8, 4*10**(-17)), color='#0E0084', fontsize=15)
 ax1.annotate('Age: <500 Myr', xy=(0.8, 2.5*10**(-17)), color='#0E0084', fontsize=15)
 ax1.annotate('$L_\mathrm{bol}: -3.254 \pm 0.004$', xy=(0.8, 1.6*10**(-17)), color='#0E0084', fontsize=15)
